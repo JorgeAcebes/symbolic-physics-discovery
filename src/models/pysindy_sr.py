33d5d47cc62c58ad98454097a845716f4d1d68e2
Jorge Acebes <jorgea2004@gmail.com>
2026-04-13 18:27:08 +0200

    comments on pysindy

--- src/models/pysindy_sr.py
+++ src/models/pysindy_sr.py
@@ -1,41 +1,43 @@
+# Importaciones para PySINDY + el modelo base para la clase
 import numpy as np
-from pysindy.optimizers import STLSQ
+from pysindy.optimizers import STLSQ # Importamos Sequential Threshold Least Squares
 from pysindy.feature_library import PolynomialLibrary, CustomLibrary, GeneralizedLibrary
 from models.base import PhysicalModel
 
 class PySINDyWrapper(PhysicalModel):
-    def __init__(self, feature_names=None, degree=2):
+    def __init__(self, feature_names=None, degree=3):
         super().__init__()
         self.feature_names = feature_names
         self.degree = degree
         
-        lib_poly = PolynomialLibrary(degree=self.degree, include_interaction=True)
+        lib_poly = PolynomialLibrary(degree=self.degree, include_interaction=True) # Creamos la base del espacio de funciones (idéntico a polynomial + 1)
         lib_custom = CustomLibrary(
             library_functions=[lambda x: 1.0 / (x + 1e-8), lambda x: 1.0 / (x**2 + 1e-8)],
             function_names=[lambda x: f"(1/{x})", lambda x: f"(1/{x}^2)"]
-        )
+        ) # Ampliamos la base del espacio de funciones añadiendo funciones racionales (1/x, 1/x^2) asegurando que no se den divergencias
         
         # Base funcional unificada
         self.library = GeneralizedLibrary([lib_poly, lib_custom])
-        self.optimizer = STLSQ(threshold=0.01)
+        self.optimizer = STLSQ(threshold=0.1) # Optimizador con L2 (Ridge) pero con un threshold en los pesos de 0.1 como mínimo
 
     def fit(self, X_train, y_train):
         # Proyección sobre el espacio de características
-        self.library.fit(X_train)
-        Theta = self.library.transform(X_train)
+        self.library.fit(X_train) # Ve cómo es X para saber cómo tendrán que ser las dimensiones de Theta. No actualiza nada 
+        Theta = self.library.transform(X_train) # Transformamos el vector X en la base del espacio de funciones (por eso necesitaba saber dimensiones)
         
         # Optimización dispersa L_0
-        self.optimizer.fit(Theta, y_train)
+        self.optimizer.fit(Theta, y_train) # El optimizador resuelve el problema y = Thteta* Xi, donde Xi es la matriz de coeficientes dispersos
         
         # Recuperación de la ecuación física
-        coefs = self.optimizer.coef_[0]
-        feat_names = self.library.get_feature_names(self.feature_names)
+        coefs = self.optimizer.coef_[0] # Extraemos el vector Xi (subíndice cero porque y podría ser un vector en vez de un escalar, y Xi entonces sería una matriz)
+        feat_names = self.library.get_feature_names(self.feature_names) # Recuperamos las etiquetas simbólicas que le corresponden a cada uno
         
-        terms = [f"{c:.3e}*{n}" for c, n in zip(coefs, feat_names) if abs(c) > 0]
-        self.equation = " + ".join(terms) if terms else "0"
+        # Obtenemos la ecuación eliminando los coeficientes que no sean ~0
+        terms = [f"{c:.3e}*{n}" for c, n in zip(coefs, feat_names) if abs(c) > 1e-8]
+        self.equation = " + ".join(terms) if terms else "0" 
         return self
 
     def predict(self, X):
-        Theta = self.library.transform(X)
-        y_pred = self.optimizer.predict(Theta)
-        return y_pred.reshape(-1, 1)
+        Theta = self.library.transform(X) # Mapeamos al espacio de funciones
+        y_pred = self.optimizer.predict(Theta) # Predecimos (ya tiene internamente la matriz de pesos por el fit)
+        return y_pred.reshape(-1, 1) # Reshape para después poder calcular MSE
